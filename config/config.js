3166d8189f7f4ec133ecbb1cc4514232ae464774
Donald Harrington <110729975+donaldharrington@users.noreply.github.com>
2022-11-30 06:22:17 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -5,8 +5,8 @@ const config = {};
 
 config.serverHost         = process.env.serverHost || "127.0.0.1";
 config.serverPort         = process.env.serverPort || 8001;
-config.shopDomain         = process.env.SHOP_DOMAIN;
-config.shopAccessToken    = process.env.ACCESS_TOKEN;
+config.shopDomain         = process.env.SHOP_DOMAIN || "https://karma-east-au.myshopify.com";
+config.shopAccessToken    = process.env.ACCESS_TOKEN || "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";
 
 
 
