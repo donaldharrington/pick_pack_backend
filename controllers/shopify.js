febdcfd2f881a56162e6a7fc04c6751f04465b13
Donald Harrington <110729975+donaldharrington@users.noreply.github.com>
2022-11-30 14:53:09 +0100

    updated 2022-11-30 02:53 PM

--- controllers/shopify.js
+++ controllers/shopify.js
@@ -2,14 +2,139 @@ import mongoose from 'mongoose';
 import { Orders } from "shopify-admin-api";
 import config from "../config/config.js";
 
+const shopDomain        = config.shopDomain;
+const shopAccessToken   = config.shopAccessToken;
+
+export const getOrderByID = async (req, res, next) => {
+  try {
+    const id = req.query.id; 
+    const service = new Orders(shopDomain, shopAccessToken);
+    const order = await service.get(id);
+
+    res.json({ 
+      status    : 1,
+      msg       : "get order via id", 
+      order     : order
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const getOrders = async (req, res, next) => {
   try {
-    const service = new Orders(config.shopDomain, config.shopAccessToken);
+    const confirmed = req.query.confirmed;
+
+    const service = new Orders(shopDomain, shopAccessToken);
     const orders = await service.list();
     res.json({ 
-      status: 1, 
-      count: orders.length,
-      orders: orders 
+      status      : 1, 
+      count       : orders.length,
+      msg         : "get total order list",
+      orders      : orders 
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getOrderCount = async (req, res, next) => {
+  try {
+    const service = new Orders(shopDomain, shopAccessToken);
+    const order_count = await service.count();
+    res.json({ 
+      status      : 1, 
+      msg         : "get total order count",
+      orderCount  : order_count 
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const openOrderByID = async (req, res, next) => {
+  try {
+    const id = req.query.id; 
+    const service = new Orders(shopDomain, shopAccessToken);
+    const order = await service.open(id);
+
+    res.json({ 
+      status    : 1,
+      msg       : "opened order", 
+      order     : order
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const closeOrderByID = async (req, res, next) => {
+  try {
+    const id = req.query.id; 
+    const service = new Orders(shopDomain, shopAccessToken);
+    const order = await service.close(id);
+
+    res.json({ 
+      status    : 1,
+      msg       : "closed order", 
+      order     : order
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteOrderByID = async (req, res, next) => {
+  try {
+    const id = req.query.id; 
+    const service = new Orders(shopDomain, shopAccessToken);
+    await service.delete(id);
+
+    res.json({ 
+      status    : 1,
+      msg       : "deleted order"
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const cancelOrderByID = async (req, res, next) => {
+  try {
+    const id = req.query.id; 
+    const service = new Orders(shopDomain, shopAccessToken);
+    await service.cancel(id, {
+      reason: "Customer"
+    });
+
+    res.json({ 
+      status    : 1,
+      msg       : "canceled order"
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateOrderByID = async (req, res, next) => {
+  try {
+    const id = req.body.id; 
+    const norder = req.body.order;
+
+    const service = new Orders(shopDomain, shopAccessToken);
+    const order = await service.close(id);
+
+    let keys = Object.keys(norder);    
+    keys.forEach(key => {
+      order = {...order, [key]: norder[key]};  
+    });
+    
+    order = await service.update(id, order);
+
+    res.json({ 
+      status    : 1,
+      msg       : "updated order", 
+      order     : order
     });
   } catch (error) {
     next(error);
--- routes/shopify/index.js
+++ routes/shopify/index.js
@@ -2,11 +2,25 @@ import express from "express";
 
 
 import {
-    getOrders
+    getOrderByID,
+    getOrders,
+    getOrderCount, 
+    openOrderByID,
+    deleteOrderByID,
+    closeOrderByID, 
+    cancelOrderByID, 
+    updateOrderByID
 } from "../../controllers/shopify.js";
 
 const router = express.Router();
 
+router.get("/get_order", getOrderByID);
 router.get("/get_orders", getOrders);
+router.get("/get_order_count", getOrderCount);
+router.get("/open_order", openOrderByID);
+router.get("/delete_order", deleteOrderByID);
+router.get("/close_order", closeOrderByID);
+router.get("/cancel_order", cancelOrderByID);
+router.post("/update_order", updateOrderByID);
 
 export default router;
