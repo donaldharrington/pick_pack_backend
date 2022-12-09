4e0d63f50651efb753459f6c8a4aa6bf6d3b64be
Donald Harrington <110729975+donaldharrington@users.noreply.github.com>
2022-12-09 09:52:31 +0100

    update 2022-12-09 )9:52 PM

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,9 @@
-import { Auth, Shops  } from "shopify-admin-api";
+import { Auth, Shops, Customers  } from "shopify-admin-api";
 import config from "../config/config.js";
+import sqlite3  from 'sqlite3';
 
 const shopDomain        = config.shopDomain;
 const shopAccessToken   = config.shopAccessToken;
-
 
 export const isValidDomain = async (req, res, next) => {
   try {
@@ -29,10 +29,212 @@ export const isValidDomain = async (req, res, next) => {
 
 export const signinUser = async (req, res, next) => {
   try {
-    res.json({msg: "Login Success"});
+    const email     = req.query.email; 
+    const password  = req.query.password;      
+    
+    const service = new Customers(shopDomain, shopAccessToken);
+    const customers = await service.list( { email: email } );
+
+    if ( customers.length < 1 ) {
+      res.json({ 
+        status    : 0,
+        msg       : "invalid email", 
+      });
+    } else {
+      if ( customers[0]["email"] != email ) {
+        res.json({ 
+          status    : 0,
+          msg       : "invalid email", 
+        });
+        return;
+      }
+
+      let db = new sqlite3.Database('./db/pickpack.db');    
+      let sql = `SELECT * FROM pp_users WHERE email= ? `;
+      db.all(sql, [email], (err, rows) => {
+        if (err) { throw err; }
+        if ( rows.length < 1 ) {
+          res.json({ 
+            status    : 0,
+            msg       : "Not register to database", 
+          });
+        } else {
+          let dpassword = rows[0]["password"];
+          if ( dpassword != password ) {
+            res.json({ 
+              status    : 0,
+              msg       : "Invalid password", 
+            });
+          } else {
+            res.json({ 
+              status    : 1,
+              msg       : "Success to login", 
+            });
+          }          
+        }  
+      });
+      db.close();    
+    } 
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllUsers = async (req, res, next) => {
+  try {
+    // open the database
+    let db = new sqlite3.Database('./db/pickpack.db');
+    
+    let sql = `SELECT * FROM pp_users`;
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      res.json({
+        msg: "get all users list",
+        users: rows
+      });
+
+    });
+    // close the database connection
+    db.close();    
   } catch (error) {
     console.log(error);
     next(error);
   }
 };
+
+export const isUser = async (req, res, next) => {
+  const email = req.query.email; 
+
+  try {
+    // open the database
+    let db = new sqlite3.Database('./db/pickpack.db');
+    
+    let sql = `SELECT * FROM pp_users WHERE email=?`;
+    db.all(sql, [email], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+
+      if ( rows.length > 0 ) {
+        res.json({
+          state: 1,
+          user: rows[0]
+        });
+      } else {
+        res.json({
+          state: 0,
+          email: email
+        });
+      }     
+
+    });
+    // close the database connection
+    db.close();    
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+export const addUser = async (req, res, next) => {
+  const full_name = req.body.full_name; 
+  const email = req.body.email; 
+  const password = req.body.password; 
+
+  if ( email == "" || password == "" ) {
+    res.json({
+      state: 0,
+      msg: "Can't empty email or password"
+    });
+    return;
+  }
+
+  try {
+    // open the database
+    let db = new sqlite3.Database('./db/pickpack.db');
+    
+    let sql = `SELECT * FROM pp_users WHERE email=?`;
+    db.all(sql, [email], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+
+      if ( rows.length > 0 ) {
+        res.json({
+          state: 0,
+          msg: "Exist email already"
+        });
+      } else {
+        sql = `INSERT INTO pp_users (full_name, email, password) VALUES (?, ?, ?)`;
+        db.all(sql, [full_name, email, password], (err, rows) => {
+          if (err) {
+            throw err;
+          }
+          res.json({
+            state: 1,
+            msg: "Added successfully"
+          });   
+        });
+      }     
+
+    });    
+    // close the database connection
+    db.close();    
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+export const updateUserPassword = async (req, res, next) => {
+  const email = req.body.email; 
+  const password = req.body.password; 
+
+  if ( email === "" || password === "" ) {
+    res.json({
+      state: 0,
+      msg: "Can't empty email or password"
+    });
+    return;
+  }
+
+  try {
+    let db = new sqlite3.Database('./db/pickpack.db');
+    
+    let sql = `SELECT * FROM pp_users WHERE email= ? `;
+    db.all(sql, [email], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+
+      if ( rows.length < 1 ) {
+        res.json({
+          state: 0,
+          msg: "Invalid email address"
+        });
+      } else {
+        sql = `UPDATE pp_users SET password= ? WHERE email= ? `;
+        db.all(sql, [password, email], (err, rows) => {
+          if (err) {
+            throw err;
+          }
+          res.json({
+            state: 1,
+            rows: sql, 
+            msg: "Updated password"
+          });   
+        });
+      }     
+    });    
+    // close the database connection
+    db.close();    
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+
 
--- routes/auth/index.js
+++ routes/auth/index.js
@@ -2,10 +2,18 @@ import express from "express";
 import { 
     isValidDomain, 
     signinUser,
+    getAllUsers,
+    isUser,
+    addUser,
+    updateUserPassword
 } from "../../controllers/auth.js";
 
 const router = express.Router();
 router.get("/is_valid_domain", isValidDomain);
-router.post("/signin", signinUser);
+router.get("/signin", signinUser);
+router.get("/get_all_users", getAllUsers);
+router.get("/is_user", isUser);
+router.post("/add_user", addUser);
+router.post("/update_user_password", updateUserPassword);
 
 export default router;
