7d90af00044c6cd21c3427eef0eab0724f12201c
Donald Harrington <110729975+donaldharrington@users.noreply.github.com>
2022-12-13 07:36:35 +0100

    Update index.js

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -8,10 +8,10 @@ import {
     updateUserPassword
 } from "../../controllers/auth.js";
 
-const router = express.Router();
-router.get("/is_valid_domain", isValidDomain);
-router.get("/signin", signinUser);
-router.get("/get_all_users", getAllUsers);
+const router = express.Router();  
+router.get("/is_valid_domain", isValidDomain);  
+router.get("/signin", signinUser);  
+router.get("/get_all_users", getAllUsers); 
 router.get("/is_user", isUser);
 router.post("/add_user", addUser);
 router.post("/update_user_password", updateUserPassword);
